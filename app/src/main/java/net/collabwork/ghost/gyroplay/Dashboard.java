2cba3ac30281a2c3a760742ed171edbcb4248664
Romain <rom.prevot@gmail.com>
2014-09-14 23:50:09 +0200

    more fun

--- app/src/main/java/net/collabwork/ghost/gyroplay/Dashboard.java
+++ app/src/main/java/net/collabwork/ghost/gyroplay/Dashboard.java
@@ -6,14 +6,24 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Arrays;
 
 
-public class Dashboard extends Activity implements SensorEventListener {
+public class Dashboard extends Activity implements SensorEventListener, View.OnClickListener {
+
+    private Socket socket;
 
     private SensorManager sensorManager;
     private Sensor sensor;
@@ -21,6 +31,8 @@ public class Dashboard extends Activity implements SensorEventListener {
     private TextView txtY;
     private TextView txtZ;
     private TextView txtT;
+    private TextView txtHost;
+    private Button btConnect;
 
     private float[] origin = new float[4];
     private boolean originInit = false;
@@ -29,14 +41,18 @@ public class Dashboard extends Activity implements SensorEventListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        txtX = (TextView)findViewById(R.id.txtX);
-        txtY = (TextView)findViewById(R.id.txtY);
-        txtZ = (TextView)findViewById(R.id.txtZ);
-        txtT = (TextView)findViewById(R.id.txtT);
+        txtX = (TextView) findViewById(R.id.txtX);
+        txtY = (TextView) findViewById(R.id.txtY);
+        txtZ = (TextView) findViewById(R.id.txtZ);
+        txtT = (TextView) findViewById(R.id.txtT);
+        txtHost = (TextView) findViewById(R.id.txtHost);
+        btConnect = (Button) findViewById(R.id.btConnect);
 
         // http://developer.android.com/guide/topics/sensors/sensors_motion.html#sensors-motion-rotate
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        btConnect.setOnClickListener(this);
     }
 
 
@@ -51,6 +67,13 @@ public class Dashboard extends Activity implements SensorEventListener {
         super.onPause();
         sensorManager.unregisterListener(this);
         originInit = false;
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException ioe) {
+                // ignored
+            }
+        }
     }
 
     @Override
@@ -74,13 +97,14 @@ public class Dashboard extends Activity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        if(!originInit){
-            origin = sensorEvent.values;
+        if (!originInit) {
+            Log.d("Ghost", "init");
+            System.arraycopy(sensorEvent.values, 0, origin, 0, 4);
             originInit = true;
         }
-        float x = sensorEvent.values[0] - origin[0];
-        float y = sensorEvent.values[1] - origin[1];
-        float z = sensorEvent.values[2] - origin[2];
+        float x = sensorEvent.values[0] - origin[0] * 100;
+        float y = sensorEvent.values[1] - origin[1] * 100;
+        float z = sensorEvent.values[2] - origin[2] * 100;
 
         txtX.setText(String.valueOf(x));
         txtY.setText(String.valueOf(y));
@@ -90,5 +114,39 @@ public class Dashboard extends Activity implements SensorEventListener {
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        String host = txtHost.getText().toString();
+        Log.d("Ghost", "trying to connect to " + host);
+        new ConnectionThread().execute(host);
+    }
+
+    private class ConnectionThread extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            final String host = strings[0];
+            try {
+                Socket socket = new Socket(host, 1337);
+                socket.getOutputStream();
+                socket.close();
+                Dashboard.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("Ghost", "Connected!");
+                    }
+                });
+            } catch (final IOException ioe) {
+                Dashboard.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("Ghost", "Error: " + ioe.getMessage());
+                    }
+                });
+            }
+            return null;
+        }
     }
 }
